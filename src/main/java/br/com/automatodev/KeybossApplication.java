f87bdd8c2711263aedf287ceb61cea46f54a0e12
Marco Aurélio <programadormoderador@hotmail.com>
2024-05-01 14:02:13 -0300

    :tada: init: Initial commit

---
+++ src/main/java/br/com/automatodev/KeybossApplication.java
@@ -0,0 +1,13 @@
+package br.com.automatodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeybossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeybossApplication.class, args);
+	}
+
+}
