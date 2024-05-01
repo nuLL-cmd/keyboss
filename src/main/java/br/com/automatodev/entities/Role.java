b91ffea94d48c4bb30faa891b3f8e2c3a3e4461a
Marco Aurélio <programadormoderador@hotmail.com>
2024-05-01 20:01:10 -0300

    :bricks: impl: security jwt implementation

--- src/main/java/br/com/automatodev/entities/Role.java
+++ src/main/java/br/com/automatodev/entities/Role.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "reoles")
+@Table(name = "roles")
 public class Role {
 
     @Id
--- src/main/java/br/com/automatodev/security/SecurityConfig.java
+++ src/main/java/br/com/automatodev/security/SecurityConfig.java
@@ -1,2 +1,63 @@
-package br.com.automatodev.security;public class SecurityConfig {
+package br.com.automatodev.security;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // All requests are authenticated
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+        //Disable only in dev environment
+        http.csrf(csrf -> csrf.disable());
+        //Use jwt
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        //Credentials will not be stored in session
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+    }
+
 }
